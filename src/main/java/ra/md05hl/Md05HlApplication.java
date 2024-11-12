5421be4b43301059b703636eaefb60e0908828ce
Hichandayo <nguyenhienthe1120@gmail.com>
2024-11-12 14:57:38 +0700

    up

---
+++ src/main/java/ra/md05hl/Md05HlApplication.java
@@ -0,0 +1,13 @@
+package ra.md05hl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md05HlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md05HlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ra/md05hl/Md05HlApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.md05hl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md05HlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
